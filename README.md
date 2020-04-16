b053301d2a630a2b08f2da214356d5fc3d2cea17
Yue-Lan <lanyue@ubuntukylin.com>
2020-04-16 14:07:51 +0800

    internal style.

--- README.md
+++ README.md
@@ -1,24 +1,18 @@
 # style-example-qt5
 qt5 style的例子
 
-## 概述
-这个项目可以用于熟悉QStyle，你也可以fork这个项目自己进行尝试和修改
+## InternalStyle
+InternalStyle是主题框架的特殊规则，用于绕开主题框架的主题切换。
 
-正如我在qt5-ukui项目的文档中所写到的，我们的设计和开发理念不能够一直停留在过去，想要把我的思路一清二楚的传达出来是很难的，我只希望当大家遇到困难的时候再回想起我的妄言，这个时候再去了解相关的知识和概念也不迟
+想要切换系统主题，可以在终端执行以下命令:
 
-这个项目只讲怎么做，什么效果。它只能作为你入门的一个参考资料，作为开发者，在真正开发一个项目时候需要有丰富的知识储备和灵活变通的能力，虽然QStyle也不是万能的，但哪怕你不用它，你也要学会灵活运用其理念进行开发，因为这是贯穿qt5的一个重要框架，你没办法在不了解它的情况下发挥qt5这个开发库的所有潜力
+> gsettings set org.ukui.style style-name ukui-default
 
-## master分支
-master本身是一个没有太多内容的分支，你可以基于master进行二次开发
 
-## 静态样式
-参考静态分支了解如何实现对一个控件样式的修改
+> gsettings set org.ukui.style style-name ukui-dark
 
-## 动画
-参考动画分支了解如何使控件和动画结合，当然动画和控件的结合比较自由，这个分支里展示的仅仅是一部分
 
-## 调色板
-参考调色板分支了解主题、控件和调色板之间的联系
+> gsettings set org.ukui.style style-name fusion
 
-## tags
-分支中的内容可能会不断更新，你可以查看tag进行比较有针对性的研究
+
+> gsettings set org.ukui.style style-name windows
--- main.cpp
+++ main.cpp
@@ -17,64 +17,24 @@
  * \param argv
  * \return
  * \see
- * CustomStyle, MainWindow
+ * InternalStyle, MainWindow
  */
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QApplication::setStyle(new CustomStyle());
+    //想使用InternalStyle绕开系统主题，就不能对qApp设置styleSheet，但是可以对控件设置qss
+//    a.setStyleSheet("QPushButton{"
+//                    "color: red;"
+//                    "}");
+
+    // fusion 是qt默认的主题，这里也可以改成ukui-default或者ukui-light
+    a.setStyle(new InternalStyle("fusion"));
+
     MainWindow w;
+    w.setStyleSheet("background-color: white");
 
-    QMainWindow settingsWindow;
-
-    /*!
-     * \brief c 用于切换主题
-     */
-    QComboBox c;
-    QStringList styles = QStyleFactory::keys();
-    styles.prepend("自定义");
-    QStringListModel model(styles);
-    c.setModel(&model);
-
-    /*!
-     * \brief c2 用于改变CustomStyle的proxy
-     */
-    QComboBox c2;
-    QStringList styles2 = QStyleFactory::keys();
-    QStringListModel model2(styles2);
-    c2.setModel(&model2);
-    c2.setCurrentText("Windows");
-
-    c.connect(&c, QOverload<int>::of(&QComboBox::currentIndexChanged), [&](int index){
-        if (index == 0) {
-            QApplication::setStyle(new CustomStyle());
-            c2.setEnabled(true);
-        } else {
-            QApplication::setStyle(model.index(index).data().toString());
-            c2.setEnabled(false);
-        }
-    });
-
-    c2.connect(&c2, QOverload<int>::of(&QComboBox::currentIndexChanged), [&](int index){
-        if (c.currentText() == "自定义") {
-            QApplication::setStyle(new CustomStyle(model2.index(index).data().toString()));
-        }
-    });
-
-    QToolBar t;
-    QLabel l("选择样式:");
-    t.addWidget(&l);
-    t.addWidget(&c);
-
-    QLabel l2("CustomStyle继承自:");
-    t.addWidget(&l2);
-    t.addWidget(&c2);
-
-    settingsWindow.addToolBar(&t);
-    settingsWindow.setCentralWidget(&w);
-
-    settingsWindow.show();
+    w.show();
 
     return a.exec();
 }
